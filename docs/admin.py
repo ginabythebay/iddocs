45ebfc84c9c4f8ca51c16e0dcc25590664635d3c
Gina White <ginabythebay@gmail.com>
2017-03-10 11:43:16 -0800

    locations work
    make it easy to preload 50 states

--- docs/admin.py
+++ docs/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from markdownx.admin import MarkdownxModelAdmin
 
-from .models import BirthCertificate
+from .models import BirthCertificate, Location
 
 admin.site.register(BirthCertificate, MarkdownxModelAdmin)
+admin.site.register(Location)
--- docs/models.py
+++ docs/models.py
@@ -7,11 +7,24 @@ from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 
 @python_2_unicode_compatible
+class Location(models.Model):
+    abbrev = models.CharField(primary_key=True, max_length=2)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
 class BirthCertificate(models.Model):
     added = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     location = models.CharField(primary_key=True, max_length=2)
     article = MarkdownxField()
+    location = models.OneToOneField(
+        Location,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
 
     @property
     def formatted_article(self):
