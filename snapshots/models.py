c946f611aea403a92babc7e26af1c918344b8dd2
Gina White <ginabythebay@gmail.com>
2017-03-23 14:44:33 -0700

    1st draft at snapshot model
    singleton, only keeping latest around.  if the snapshot is found, we
    display it in the index view.

--- snapshots/models.py
+++ snapshots/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
 
+from django.utils.encoding import python_2_unicode_compatible
+
 from django.db import models
 
-# Create your models here.
+@python_2_unicode_compatible
+class Snapshot(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        Snapshot.objects.exclude(id=self.id).delete()
+        super(Snapshot, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.added)
+
+
+#possible states:
+#  snapshot never done
+#  snapshot done, never published
+#  snapshot was published, never another snapshot
+#  snapshot was published, newer snapshot made
+#
+#  maybe we have snapshot
+#    number
+#    creation time
+#
+#  publication
+#    snapshot number
+#    snapshot time
+#    creation time
--- snapshots/views.py
+++ snapshots/views.py
@@ -3,13 +3,19 @@ from django.core.management import call_command
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
+from .models import Snapshot
 
 class IndexView(TemplateView):
     template_name = 'snapshots/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+
+        snap_query = Snapshot.objects.all()[:1]
+        snap = snap_query[0] if len(snap_query) == 1 else None
+
         context.update({
+            'snap': snap,
             'messages': messages.get_messages(self.request),
         })
         return context
@@ -23,5 +29,6 @@ def create(request):
     #   * fork bakery
     #   * embed the relevant bits of the command into my own source
     call_command('build')
+    Snapshot().save()
     messages.add_message(request, messages.INFO, 'Snapshot Created.')
     return redirect('snapshots:index')
