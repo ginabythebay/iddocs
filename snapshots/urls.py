b24c8d7a367a2b792d60d3b08823ee3315b0cca1
Gina White <ginabythebay@gmail.com>
2017-03-26 12:27:41 -0700

    snapshots auth work
    wrap snapshots views in admin.site.admin_view which ensures that the\
    user is a staff user and redirects to the correct login page if they
    are not logged in.

--- snapshots/urls.py
+++ snapshots/urls.py
@@ -1,10 +1,11 @@
+from django.contrib import admin
 from django.conf.urls import url
 
 from . import views
 
-app_name='snapshots'
+app_name = 'snapshots'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^create/$', views.create, name='create'),
-    url(r'^publish/$', views.publish, name='publish'),
+    url(r'^$', admin.site.admin_view(views.IndexView.as_view()), name='index'),
+    url(r'^create/$', admin.site.admin_view(views.create), name='create'),
+    url(r'^publish/$', admin.site.admin_view(views.publish), name='publish'),
 ]
